c212c551493c978540bac302ce5bb77d2274d4bc
TMax66 <massimo.tranquillo@gmail.com>
2021-10-31 23:14:07 +0100

    update

--- global.R
+++ global.R
@@ -69,9 +69,11 @@ library(hrbrthemes)
 
 # saveRDS(covid, here("data", "processed",  "covid.rds"))
 
+# Dati
 covid <- readRDS(here("data", "processed", "covid.rds"))
 
-
+# Funzioni
+## Valuebox
 valueBox <- function(value, subtitle, icon, color) {
   div(class = "col-lg-3 col-md-6",
       div(class = "panel panel-primary",
@@ -93,4 +95,75 @@ valueBox <- function(value, subtitle, icon, color) {
           )
       )
   )
+}
+
+
+## Plot
+serie1 <- function(){  
+  covid %>% 
+    filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
+    mutate(anno = year(dtacc)) %>% 
+    filter(anno == 2021) %>% 
+    
+    
+    group_by(dtacc) %>% 
+    summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
+    filter(esami > 0) %>%
+    mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
+    ggplot(aes(
+      x = dtacc, 
+      y = sett
+    ))+
+    geom_line(col = "blue", size = 1.5)+
+    geom_point(aes(x = dtacc, 
+                   y = esami), alpha = 1/5)+
+    geom_line(aes(x = dtacc, 
+                  y = esami), alpha = 1/5)+
+    
+    labs(
+      y = "Numero Tamponi Naso Faringei", 
+      x = "", 
+      title = "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
+      subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
+    )+
+    theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
+                   plot_title_size = 10)+
+    theme(
+      axis.text.x=element_text(size = 10))
+}
+
+serie2 <- function(regione){  
+  covid %>% 
+    filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
+    mutate(anno = year(dtacc)) %>% 
+    filter(anno == 2021 & Regione == regione) %>% 
+    group_by(dtacc) %>% 
+    summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
+    filter(esami > 0) %>%
+    mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
+    ggplot(aes(
+      x = dtacc, 
+      y = sett
+    ))+
+    geom_line(col = "blue", size = 1.5)+
+    # geom_col( aes(y = Tot), 
+    #    alpha = 1/5)+
+    
+    geom_point(aes(x = dtacc, 
+                   y = esami), alpha = 1/5)+
+    geom_line(aes(x = dtacc, 
+                  y = esami), alpha = 1/5)+
+    #geom_hline(yintercept = 2729) +
+    
+    labs(
+      y = "Numero Tamponi Naso Faringei", 
+      x = "", 
+      title =  "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
+      subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
+    )+
+    theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
+                   plot_title_size = 10)+
+    theme(
+      axis.text.x=element_text(size = 10))
+  
 }
--- server.R
+++ server.R
@@ -14,169 +14,285 @@ server<-function(input, output) {
     
     if(input$Regione == "Dati Complessivi") {
       
-      covid %>% 
-        filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
-        mutate(anno = year(dtacc)) %>% 
-        filter(anno == 2021) %>% 
-        group_by(dtacc) %>% 
-        summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
-        filter(esami > 0) %>%
-        mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
-        ggplot(aes(
-          x = dtacc, 
-          y = sett
-        ))+
-        geom_line(col = "blue", size = 1.5)+
-        geom_point(aes(x = dtacc, 
-                       y = esami), alpha = 1/5)+
-        geom_line(aes(x = dtacc, 
-                      y = esami), alpha = 1/5)+
-        
-        labs(
-          y = "Numero Tamponi Naso Faringei", 
-          x = "", 
-          title = "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
-          subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
-        )+
-        theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
-                       plot_title_size = 10)+
-        theme(
-          axis.text.x=element_text(size = 10))
+      # covid %>% 
+      #   filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
+      #   mutate(anno = year(dtacc)) %>% 
+      #   filter(anno == 2021) %>% 
+      #   group_by(dtacc) %>% 
+      #   summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
+      #   filter(esami > 0) %>%
+      #   mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
+      #   ggplot(aes(
+      #     x = dtacc, 
+      #     y = sett
+      #   ))+
+      #   geom_line(col = "blue", size = 1.5)+
+      #   geom_point(aes(x = dtacc, 
+      #                  y = esami), alpha = 1/5)+
+      #   geom_line(aes(x = dtacc, 
+      #                 y = esami), alpha = 1/5)+
+      #   
+      #   labs(
+      #     y = "Numero Tamponi Naso Faringei", 
+      #     x = "", 
+      #     title = "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
+      #     subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
+      #   )+
+      #   theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
+      #                  plot_title_size = 10)+
+      #   theme(
+      #     axis.text.x=element_text(size = 10))
+      
+      serie1()
       
     } else  
       
       if(input$Regione == "Lombardia") {  
         
-      covid %>% 
-          filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
-      mutate(anno = year(dtacc)) %>% 
-        filter(anno == 2021 & Regione == "Lombardia") %>% 
-      group_by(dtacc) %>% 
-      summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
-      filter(esami > 0) %>%
-      mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
-      ggplot(aes(
-        x = dtacc, 
-        y = sett
-      ))+
-      geom_line(col = "blue", size = 1.5)+
-      # geom_col( aes(y = Tot), 
-      #    alpha = 1/5)+
-      
-      geom_point(aes(x = dtacc, 
-                     y = esami), alpha = 1/5)+
-      geom_line(aes(x = dtacc, 
-                    y = esami), alpha = 1/5)+
-      #geom_hline(yintercept = 2729) +
-      
-      labs(
-        y = "Numero Tamponi Naso Faringei", 
-        x = "", 
-        title =  "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
-        subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
-      )+
-          theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
-                         plot_title_size = 10)+
-          theme(
-            axis.text.x=element_text(size = 10))
+      # covid %>% 
+      #     filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
+      # mutate(anno = year(dtacc)) %>% 
+      #   filter(anno == 2021 & Regione == "Lombardia") %>% 
+      # group_by(dtacc) %>% 
+      # summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
+      # filter(esami > 0) %>%
+      # mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
+      # ggplot(aes(
+      #   x = dtacc, 
+      #   y = sett
+      # ))+
+      # geom_line(col = "blue", size = 1.5)+
+      # # geom_col( aes(y = Tot), 
+      # #    alpha = 1/5)+
+      # 
+      # geom_point(aes(x = dtacc, 
+      #                y = esami), alpha = 1/5)+
+      # geom_line(aes(x = dtacc, 
+      #               y = esami), alpha = 1/5)+
+      # #geom_hline(yintercept = 2729) +
+      # 
+      # labs(
+      #   y = "Numero Tamponi Naso Faringei", 
+      #   x = "", 
+      #   title =  "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
+      #   subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
+      # )+
+      #     theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
+      #                    plot_title_size = 10)+
+      #     theme(
+      #       axis.text.x=element_text(size = 10))
+      #   
         
+        serie2(regione = "Lombardia")
     
       } else
         
         if(input$Regione == "Emilia Romagna")
         {
           
-          covid %>% 
-            filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
-            mutate(anno = year(dtacc)) %>% 
-            filter(anno == 2021 & Regione == "Emilia Romagna") %>% 
-            group_by(dtacc) %>% 
-            summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
-            filter(esami > 0) %>%
-            mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
-            ggplot(aes(
-              x = dtacc, 
-              y = sett
-            ))+
-            geom_line(col = "blue", size = 1.5)+
-            # geom_col( aes(y = Tot), 
-            #    alpha = 1/5)+
-            
-            geom_point(aes(x = dtacc, 
-                           y = esami), alpha = 1/5)+
-            geom_line(aes(x = dtacc, 
-                          y = esami), alpha = 1/5)+
-            #geom_hline(yintercept = 2729) +
-            
-            labs(
-              y = "Numero Tamponi Naso Faringei", 
-              x = "", 
-              title =  "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
-              subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
-            )+
-            theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
-                           plot_title_size = 10)+
-            theme(
-              axis.text.x=element_text(size = 10))
-        }
+        #   covid %>% 
+        #     filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico")) %>% 
+        #     mutate(anno = year(dtacc)) %>% 
+        #     filter(anno == 2021 & Regione == "Emilia Romagna") %>% 
+        #     group_by(dtacc) %>% 
+        #     summarise(esami = sum(Tot_Eseguiti, na.rm = T)) %>%  
+        #     filter(esami > 0) %>%
+        #     mutate(sett = rollmean(esami, k = 30, fill = NA) )%>%  
+        #     ggplot(aes(
+        #       x = dtacc, 
+        #       y = sett
+        #     ))+
+        #     geom_line(col = "blue", size = 1.5)+
+        #     # geom_col( aes(y = Tot), 
+        #     #    alpha = 1/5)+
+        #     
+        #     geom_point(aes(x = dtacc, 
+        #                    y = esami), alpha = 1/5)+
+        #     geom_line(aes(x = dtacc, 
+        #                   y = esami), alpha = 1/5)+
+        #     #geom_hline(yintercept = 2729) +
+        #     
+        #     labs(
+        #       y = "Numero Tamponi Naso Faringei", 
+        #       x = "", 
+        #       title =  "Andamento del numero di tamponi naso-faringei processati dai laboratori COVID dell'IZSLER nel 2021", 
+        #       subtitle = " I punti rappresentano il numero di tamponi giornalieri, la linea blu la media mobile mensile"
+        #     )+
+        #     theme_ipsum_rc(base_size = 10,  axis_title_size = 10, 
+        #                    plot_title_size = 10)+
+        #     theme(
+        #       axis.text.x=element_text(size = 10))
+        # }
+        # 
         
-        
-        
+        serie2(regione = "Emilia Romagna")
     
   }   
     
-    )
+    })
   
       
     
   
-## Valuebox
+## Valuebox----
   
-  
-  tot <-  reactive(covid %>% 
-    mutate(anno = year(dtacc)) %>% 
-    filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
-    summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) )
-  
+## numero tamponi processati----
   output$tottamp <- renderText({
+    
+    if(input$Regione == "Dati Complessivi") {
+    
+    tot <-covid %>% 
+            mutate(anno = year(dtacc)) %>% 
+             filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
+               summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) 
    
-    tot()$esami
+    tot$esami
+    
+    } else 
+      
+      if(input$Regione == "Lombardia") { 
+        
+        tot <-covid %>% 
+          filter(Regione == "Lombardia") %>% 
+          mutate(anno = year(dtacc)) %>% 
+          filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
+          summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) 
+        
+        tot$esami
+        
+        } else
+          
+        if(input$Regione == "Emilia Romagna") {
+          
+          tot <-covid %>% 
+            filter(Regione == "Emilia Romagna") %>% 
+          mutate(anno = year(dtacc)) %>% 
+            filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
+            summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) 
+          
+          tot$esami
+          
+        }
     
   })
-  
-  
-   media <-  reactive(covid %>% 
-                     mutate(anno = year(dtacc)) %>% 
-                     filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
-                       group_by(dtacc) %>% 
-                       summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) %>% 
-                       summarise(media = mean(esami)))
-  
-   output$medg<- renderText({
+
+## media tamponi processati----
+  output$medg<- renderText({  
      
-     media()$media
+     if(input$Regione == "Dati Complessivi") {
      
+    media <-  covid %>% 
+       mutate(anno = year(dtacc)) %>% 
+        filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
+         group_by(dtacc) %>% 
+         summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) %>% 
+         summarise(media = mean(esami))
+  
+     media$media
+     
+     } else
+       
+       if(input$Regione == "Lombardia") {
+         
+         media <-  covid %>% 
+           filter(Regione == "Lombardia") %>% 
+           mutate(anno = year(dtacc)) %>% 
+           filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
+           group_by(dtacc) %>% 
+           summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) %>% 
+           summarise(media = mean(esami))
+         media$media
+       } else
+         
+      if(input$Regione == "Emilia Romagna") { 
+        
+        media <-  covid %>% 
+          filter(Regione == "Emilia Romagna") %>% 
+          mutate(anno = year(dtacc)) %>% 
+        filter(Prova %in% c("Agente eziologico", "SARS-CoV-2: agente eziologico") & anno == 2021) %>% 
+          group_by(dtacc) %>% 
+          summarise(esami = sum(Tot_Eseguiti, na.rm = TRUE)) %>% 
+          summarise(media = mean(esami))
+        media$media
+        }
    })
   
-   
-   var <- reactive(covid %>% 
-              mutate(anno = year(dtacc)) %>% 
-              filter(Prova %in% c( "SARS-CoV-2: identificazione varianti", 
-                                   "SARS-CoV-2: identificazione varianti N501Y") & anno == 2021) %>% 
-              summarise(var = sum(Tot_Eseguiti, na.rm = TRUE)))
-   
+## Varianti----
    output$vart<- renderText({
-     var()$var})
+     
+     if(input$Regione == "Dati Complessivi"){
+       var <- covid %>% 
+         mutate(anno = year(dtacc)) %>% 
+         filter(Prova %in% c( "SARS-CoV-2: identificazione varianti", 
+                              "SARS-CoV-2: identificazione varianti N501Y") & anno == 2021) %>% 
+         summarise(var = sum(Tot_Eseguiti, na.rm = TRUE))
+       var$var
+       
+         } else
+           
+      if(input$Regione == "Lombardia") {
+        var <- covid %>% 
+          filter(Regione == "Lombardia") %>% 
+          mutate(anno = year(dtacc)) %>% 
+          filter(Prova %in% c( "SARS-CoV-2: identificazione varianti", 
+                               "SARS-CoV-2: identificazione varianti N501Y") & anno == 2021) %>% 
+          summarise(var = sum(Tot_Eseguiti, na.rm = TRUE))
+        var$var
+      } else
+        
+        if(input$Regione == "Emilia Romagna") {
+          
+          var <- covid %>% 
+            filter(Regione == "Emilia Romagna") %>% 
+            mutate(anno = year(dtacc)) %>% 
+            filter(Prova %in% c( "SARS-CoV-2: identificazione varianti", 
+                                 "SARS-CoV-2: identificazione varianti N501Y") & anno == 2021) %>% 
+            summarise(var = sum(Tot_Eseguiti, na.rm = TRUE))
+          var$var
+        }
+     
+   })
+     
+## Sequenziamento
+ 
    
-   seq <- reactive(covid %>% 
-                     mutate(anno = year(dtacc)) %>% 
-                     filter(Prova %in% c( "Sequenziamento acidi nucleici", 
-                                          "Sequenziamento genomico SARS-CoV-2 - Illumina - Miseq",
-                                          "Sequenziamento genomico SARS-CoV-2 - Illumina - Nextseq") & anno == 2021) %>% 
-                     summarise(seq = sum(Tot_Eseguiti, na.rm = TRUE)))
-   
-   output$seqt<- renderText({
-     seq()$seq})
+output$seqt<- renderText({
+  if(input$Regione == "Dati Complessivi"){
+    seq <- covid %>% 
+      mutate(anno = year(dtacc)) %>% 
+      filter(Prova %in% c( "Sequenziamento acidi nucleici", 
+                           "Sequenziamento genomico SARS-CoV-2 - Illumina - Miseq",
+                           "Sequenziamento genomico SARS-CoV-2 - Illumina - Nextseq") & anno == 2021) %>% 
+      summarise(seq = sum(Tot_Eseguiti, na.rm = TRUE))
+    seq$seq
+  } else
+    if(input$Regione == "Lombardia"){
+      seq <- covid %>% 
+        filter(Regione == "Lombardia") %>% 
+        mutate(anno = year(dtacc)) %>% 
+        filter(Prova %in% c( "Sequenziamento acidi nucleici", 
+                             "Sequenziamento genomico SARS-CoV-2 - Illumina - Miseq",
+                             "Sequenziamento genomico SARS-CoV-2 - Illumina - Nextseq") & anno == 2021) %>% 
+        summarise(seq = sum(Tot_Eseguiti, na.rm = TRUE))
+      seq$seq
+    } else
+      if(input$Regione == "Emilia Romagna"){
+        seq <- covid %>% 
+          filter(Regione == "Emilia Romagna") %>% 
+          mutate(anno = year(dtacc)) %>% 
+          filter(Prova %in% c( "Sequenziamento acidi nucleici", 
+                               "Sequenziamento genomico SARS-CoV-2 - Illumina - Miseq",
+                               "Sequenziamento genomico SARS-CoV-2 - Illumina - Nextseq") & anno == 2021) %>% 
+          summarise(seq = sum(Tot_Eseguiti, na.rm = TRUE))
+        seq$seq 
+      }
+
+})
+     
+     
+     
+     
+     
    
    
    
