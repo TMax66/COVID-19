8ef1bf00890562cc08cf085ee9e8b779b023ef0e
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-01-13 15:22:47 +0100

    update

--- preparazione dati.R
+++ preparazione dati.R
@@ -10,9 +10,9 @@ library(lubridate)
 conn <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "dbprod02.izsler.it",Database = "IZSLER", Port = 1433)
 # #
 # #
-queryCovid <- ("SELECT        Conferimenti.Numero AS nconf, Anag_TipoConf.Descrizione AS tipoconf, Anag_Comuni.Provincia, Anag_Referenti.Ragione_Sociale AS Conferente, Anag_Finalita.Descrizione AS Finalità, Anag_Regioni.Descrizione AS Regione, 
+queryCovid <- "SELECT        Conferimenti.Numero AS nconf, Anag_TipoConf.Descrizione AS tipoconf, Anag_Comuni.Provincia, Anag_Referenti.Ragione_Sociale AS Conferente, Anag_Finalita.Descrizione AS Finalità, Anag_Regioni.Descrizione AS Regione, 
                          Anag_Comuni.Descrizione AS Comune, Anag_Materiali.Descrizione AS Materiale, Anag_Prove.Descrizione AS Prova, Anag_Referenti.Codice AS codiceconf, Conferimenti.Data AS dtconf, Conferimenti.Data_Accettazione AS dtacc, 
-                         RDP_Date_Emissione.Data_RDP AS dtref, Anag_Reparti.Descrizione AS Reparto, Esami_Aggregati.Tot_Eseguiti, dbo_Anag_Referenti_DestFatt.Ragione_Sociale, Anag_Referenti_1.Pubblico
+                         RDP_Date_Emissione.Data_RDP AS dtref, Anag_Reparti.Descrizione AS Reparto, Esami_Aggregati.Tot_Eseguiti, dbo_Anag_Referenti_DestFatt.Ragione_Sociale, Anag_Referenti.Pubblico AS Pubblico
 FROM            Anag_TipoConf INNER JOIN
                          Conferimenti ON Anag_TipoConf.Codice = Conferimenti.Tipo INNER JOIN
                          Anag_Comuni ON Anag_Comuni.Codice = Conferimenti.Luogo_Prelievo LEFT OUTER JOIN
@@ -32,13 +32,11 @@ FROM            Anag_TipoConf INNER JOIN
                          Anag_Referenti AS dbo_Anag_Referenti_DestFatt ON Conferimenti.Dest_Fattura = dbo_Anag_Referenti_DestFatt.Codice LEFT OUTER JOIN
                          Anag_Materiali ON Anag_Materiali.Codice = Conferimenti.Codice_Materiale INNER JOIN
                          Conferimenti_Finalita ON Conferimenti.Anno = Conferimenti_Finalita.Anno AND Conferimenti.Numero = Conferimenti_Finalita.Numero INNER JOIN
-                         Anag_Finalita AS dbo_Anag_Finalita_Confer ON Conferimenti_Finalita.Finalita = dbo_Anag_Finalita_Confer.Codice INNER JOIN
-                         Anag_Referenti AS Anag_Referenti_1 ON Anag_Comuni.Codice = Anag_Referenti_1.Comune LEFT OUTER JOIN
+                         Anag_Finalita AS dbo_Anag_Finalita_Confer ON Conferimenti_Finalita.Finalita = dbo_Anag_Finalita_Confer.Codice LEFT OUTER JOIN
                          RDP_Date_Emissione ON RDP_Date_Emissione.Anno = Conferimenti.Anno AND RDP_Date_Emissione.Numero = Conferimenti.Numero
 WHERE        (Laboratori_Reparto.Laboratorio > 1) AND (Esami_Aggregati.Esame_Altro_Ente = 0) AND (Esami_Aggregati.Esame_Altro_Ente = 0) AND (dbo_Anag_Finalita_Confer.Descrizione IN ('Emergenza COVID-19', 'Varianti SARS-CoV2')) 
                          AND (Anag_Prove.Descrizione NOT IN ('Motivazione di inidoneità campione', 'Motivi di mancata esecuzione di prove richieste', 'Motivi di riemissione del Rapporto di Prova', 'Note alle prove', 
                          'Opinioni ed interpretazioni -non oggetto dell''accredit. ACCREDIA'))"
-)
 
 
 
@@ -54,6 +52,9 @@ covid[,"Comune"] <- sapply(covid[, "Comune"], iconv, from = "latin1", to = "UTF-
 
 covid <- covid %>% 
   mutate(anno = year(dtacc))
+
+covid$Pubblico <- ifelse(covid$Pubblico == "TRUE", "Pubblico", "Privato")
+
 #
 saveRDS(covid, here("data", "processed",  "covid.rds"))
 
