fbdc55ea4252362a7ecd0f6d14a4d6b43759bc54
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2023-01-26 11:39:57 +0100

    update

--- R/Codici per presentazione covid.R
+++
@@ -1,7 +0,0 @@
-librerie()
-library(openxlsx)
-dt <- readRDS(here("data", "processed", "covid.rds"))
-
-data.frame( conferente =levels(factor(dt$Conferente))) %>% 
-  write.xlsx(file = "conferenti.xlsx")
-
